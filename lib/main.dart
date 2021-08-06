77962e391de815c217ed1a98dace8498f8886e23
Niraj Ballav Pant <nirajbpant@gmail.com>
2021-08-06 12:38:07 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.purple,
+        accentColor: Colors.deepOrange,
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
